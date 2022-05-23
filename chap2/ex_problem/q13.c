cff151b65d02c5efe85d06d56b301dc9bdfe2608
seonghwi <hwi7141@gmail.com>
2022-05-23 17:50:11 +0900

    progress

--- chap2/ex_problem/q13.c
+++ chap2/ex_problem/q13.c
@@ -6,6 +6,19 @@ typedef	struct {
 	int	d;
 }	Date;
 
+Date Before(Date x, int n);
+Date After(Date x, int n);
+
+static int mdays[2][12] = {
+	{ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 },
+	{ 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }
+};
+
+int isleap(int year)
+{
+	return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+}
+
 Date	Dateof(int y, int m, int d)
 {
 	Date	ret;
@@ -19,4 +32,34 @@ Date	Dateof(int y, int m, int d)
 
 Date	After(Date x, int n)
 {
-	
+	if (n < 0)
+		return Before(x, -n);
+
+	x.d += n;
+
+	while (x.d > mdays[isleap(x.y)][x.m - 1]) {
+		x.d -= mdays[isleap(x.y)][x.m - 1];
+		if (++x.m > 12) {
+			x.y++;
+			x.m = 1;
+		}
+	}
+	return (x);
+}
+
+Date	Before(Date x, int n)
+{
+	if (n < 0)
+		return After(x, -n);
+
+	x.d -= n;
+
+	while (x.d < 1) {
+		if (--x.m < 1) {
+			x.y--;
+			x.m = 12;
+		}
+		x.d += mdays[isleap(x.y)][x.m - 1];
+	}
+	return (x);
+}
