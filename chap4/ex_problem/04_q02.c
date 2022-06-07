53b5cde909f2c6d835bb898c4ebf87688c8b54b9
seonghwi <hwi7141@gmail.com>
2022-06-07 21:24:46 +0900

    progress

--- chap4/ex_problem/04_q02.c
+++ chap4/ex_problem/04_q02.c
@@ -37,25 +37,42 @@ int Push_r(IntStack *s, int x)
     return (0);
 }
 
-int Pop(IntStack *s, int *x)
+int Pop_l(IntStack *s, int *x)
 {
-    if(s->ptr <= 0)
+    if(s->l_ptr <= 0)
         return (-1);
-    *x = s->stk[--s->ptr];
+    *x = s->stk[--s->l_ptr];
     return (0);
 }
 
-int Peek(const IntStack *s, int *x)
+int Pop_r(IntStack *s, int *x)
 {
-    if(s->ptr <= 0)
+    if(s->r_ptr <= 0)
         return (-1);
-    *x = s->stk[s->ptr - 1];
+    *x = s->stk[--s->r_ptr];
+    return (0);
+}
+
+int Peek_l(const IntStack *s, int *x)
+{
+    if(s->l_ptr <= 0)
+        return (-1);
+    *x = s->stk[s->l_ptr - 1];
+    return (0);
+}
+
+int Peek_r(const IntStack *s, int *x)
+{
+    if(s->r_ptr <= 0)
+        return (-1);
+    *x = s->stk[s->max - s->r_ptr];
     return (0);
 }
 
 void Clear(IntStack *s)
 {
-    s->ptr = 0;
+    s->l_ptr = 0;
+    s->r_ptr = 0;
 }
 
 int Capacity(const IntStack *s)
@@ -63,25 +80,47 @@ int Capacity(const IntStack *s)
     return (s->max);
 }
 
-int Size(const IntStack *s)
+int Size_l(const IntStack *s)
 {
-    return (s->ptr);
+    return (s->l_ptr);
 }
 
-int IsEmpty(const IntStack *s)
+int Size_r(const IntStack *s)
 {
-    return (s->ptr <= 0);
+    return (s->r_ptr);
 }
 
-int IsFull(const IntStack *s)
+int IsEmpty_l(const IntStack *s)
 {
-    return (s->ptr >= s->max);
+    return (s->l_ptr <= 0);
 }
+
+int IsEmpty_r(const IntStack *s)
+{
+    return (s->r_ptr <= 0);
+}
+
+
+int IsFull_l(const IntStack *s)
+{
+    return (s->l_ptr >= s->max);
+}
+
+int IsFull_r(const IntStack *s)
+{
+    return (s->r_ptr >= s->max);
+}
+
 
 int Search(const IntStack *s, int x)
 {
     int i;
-    for(i = s->ptr - 1; i >= 0; i--)
+    for(i = s->l_ptr - 1; i >= 0; i--)
+    {
+        if(s->stk[i] == x)
+            return (i);
+    }
+    for(i = s->max - s->r_ptr; i < s->max; i++)
     {
         if(s->stk[i] == x)
             return (i);
@@ -89,10 +128,18 @@ int Search(const IntStack *s, int x)
     return (-1);
 }
 
-void Print(const IntStack *s)
+void Print_l(const IntStack *s)
 {
     int i;
-    for(i = 0; i < s->ptr; i++)
+    for(i = 0; i < s->l_ptr; i++)
+        printf("%d ", s->stk[i]);
+    putchar('\n');
+}
+
+void Print_r(const IntStack *s)
+{
+    int i;
+    for(i = s->max - s->r_ptr; i < s->max; i++)
         printf("%d ", s->stk[i]);
     putchar('\n');
 }
@@ -101,5 +148,5 @@ void Terminate(IntStack *s)
 {
     if(s->stk != NULL)
         free(s->stk);
-    s->max = s->ptr = 0;
+    s->max = s->l_ptr = s->r_ptr = 0;
 }
